3416c15b3d916843ecdb82ceed86c5dde6027caf
Cleyson Lima <cleysonph@gmail.com>
2021-04-06 15:31:18 -0300

    create base authors strcture classes

---
+++ src/main/java/com/cleysonph/bookstoremanager/author/mapper/AuthorMapper.java
@@ -0,0 +1,18 @@
+package com.cleysonph.bookstoremanager.author.mapper;
+
+import com.cleysonph.bookstoremanager.author.dto.AuthorDTO;
+import com.cleysonph.bookstoremanager.author.entity.Author;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AuthorMapper {
+
+    AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
+
+    Author toModel(AuthorDTO authorDTO);
+
+    AuthorDTO toDTO(Author author);
+
+}
