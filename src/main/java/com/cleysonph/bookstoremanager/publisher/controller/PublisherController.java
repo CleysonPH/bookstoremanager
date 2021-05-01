d48295c20eac2aec569b2d1976d9197dc03a341a
Cleyson Lima <cleysonph@gmail.com>
2021-05-01 18:51:57 -0300

    developed find publisher by id feature

--- src/main/java/com/cleysonph/bookstoremanager/publisher/controller/PublisherController.java
+++ src/main/java/com/cleysonph/bookstoremanager/publisher/controller/PublisherController.java
@@ -7,6 +7,8 @@ import com.cleysonph.bookstoremanager.publisher.service.PublisherService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,11 @@ public class PublisherController implements PublisherControllerDocs {
     @ResponseStatus(code = HttpStatus.CREATED)
     public PublisherDTO create(@RequestBody @Valid PublisherDTO publisherDTO) {
         return publisherService.create(publisherDTO);
+    }
+
+    @GetMapping("/{id}")
+    public PublisherDTO findById(@PathVariable Long id) {
+        return publisherService.findById(id);
     }
 
 }
