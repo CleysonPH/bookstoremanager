be5e2a79b941f34a6b7f0e83b3b10be75c9a22dc
Cleyson Lima <cleysonph@gmail.com>
2021-05-01 17:28:30 -0300

    developed publisher creation feature

---
+++ src/main/java/com/cleysonph/bookstoremanager/publisher/exception/PublisherAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.cleysonph.bookstoremanager.publisher.exception;
+
+import javax.persistence.EntityExistsException;
+
+public class PublisherAlreadyExistsException extends EntityExistsException {
+
+    public PublisherAlreadyExistsException(String name, String code) {
+        super(String.format("Publisher with name %s or code %s already exists!", name, code));
+    }
+
+}
--- src/test/java/com/cleysonph/bookstoremanager/publisher/service/PublisherServiceTest.java
+++ src/test/java/com/cleysonph/bookstoremanager/publisher/service/PublisherServiceTest.java
@@ -1,10 +1,22 @@
 package com.cleysonph.bookstoremanager.publisher.service;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import com.cleysonph.bookstoremanager.publisher.builder.PublisherDTOBuilder;
+import com.cleysonph.bookstoremanager.publisher.dto.PublisherDTO;
+import com.cleysonph.bookstoremanager.publisher.entity.Publisher;
+import com.cleysonph.bookstoremanager.publisher.exception.PublisherAlreadyExistsException;
 import com.cleysonph.bookstoremanager.publisher.mapper.PublisherMapper;
 import com.cleysonph.bookstoremanager.publisher.repository.PublisherRepository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -26,6 +38,38 @@ public class PublisherServiceTest {
     @BeforeEach
     void setUp() {
         publisherDTOBuilder = PublisherDTOBuilder.builder().build();
+    }
+
+    @Test
+    void whenNewPublisherIsInformedTheItShouldBeCreated() {
+        PublisherDTO expectedPublisherToCreateDTO = publisherDTOBuilder.buildPublisherDTO();
+        Publisher expectedPublisherCreated = PUBLISHER_MAPPER.toModel(expectedPublisherToCreateDTO);
+
+        when(publisherRepository.findByNameOrCode(expectedPublisherToCreateDTO.getName(), expectedPublisherToCreateDTO.getCode()))
+            .thenReturn(Optional.empty());
+        when(publisherRepository.save(expectedPublisherCreated)).thenReturn(expectedPublisherCreated);
+
+        PublisherDTO createdPublisherDTO = publisherService.create(expectedPublisherToCreateDTO);
+
+        assertThat(createdPublisherDTO, is(equalTo(expectedPublisherToCreateDTO)));
+    }
+
+    @Test
+    void whenExistingPublisherIsInformedThenAnExceptonShouldBeThrown() {
+        PublisherDTO expectedPublisherToCreateDTO = publisherDTOBuilder.buildPublisherDTO();
+        Publisher expectedPublisherDuplicated = PUBLISHER_MAPPER.toModel(expectedPublisherToCreateDTO);
+
+        when(publisherRepository.findByNameOrCode(expectedPublisherToCreateDTO.getName(), expectedPublisherToCreateDTO.getCode()))
+            .thenReturn(Optional.of(expectedPublisherDuplicated));
+
+        try {
+            publisherService.create(expectedPublisherToCreateDTO);
+            fail("Expected an PublisherAlreadyExistsException to be thrown");
+        } catch (PublisherAlreadyExistsException e) {
+            String name = expectedPublisherToCreateDTO.getName();
+            String code = expectedPublisherToCreateDTO.getCode();
+            assertThat(e.getMessage(), is(String.format("Publisher with name %s or code %s already exists!", name, code)));
+        }
     }
 
 }
