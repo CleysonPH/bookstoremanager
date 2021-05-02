3a8d55b987d1cdbf126afe45d1697aca01f9e675
Cleyson Lima <cleysonph@gmail.com>
2021-05-02 19:59:22 -0300

    create the user dto class

---
+++ src/main/java/com/cleysonph/bookstoremanager/user/dto/UserDTO.java
@@ -0,0 +1,58 @@
+package com.cleysonph.bookstoremanager.user.dto;
+
+import java.time.LocalDate;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.cleysonph.bookstoremanager.user.enums.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Max(120)
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Gender gender;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    @NotNull
+    @JsonFormat(shape = Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate birthDate;
+
+}
